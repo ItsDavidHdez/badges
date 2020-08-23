51bc00d0a84b4533617e8bc87a6296115c339a6d
David Vargas Hernandez <david-0610@live.com.mx>
2020-08-23 02:59:27 -0500

    Página Not Found creada

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import '../pages/styles/NotFound.css';
+import Navbar from '../components/Navbar';
+import NotFoundImg from '../images/error.svg'  
 
-function NotFound() {
-    return <h1>404: Not Found</h1>
+class NotFound extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar/>
+                <div className="NotFound__hero">
+                    <div className="NotFound__main">
+                        <img className="NotFound__main-img" src={NotFoundImg}></img>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    };
 }
 
-export default NotFound
+export default NotFound;
