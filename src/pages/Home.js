e38e2d217c79f6a99e7dad26c6d5ed8e2782a081
David Vargas Hernandez <david-0610@live.com.mx>
2020-08-23 02:57:43 -0500

    Página Home creada

---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../pages/styles/Home.css';
+import confLogo from '../images/platziconf-logo.svg';
+import Navbar from '../components/Navbar';
+import Astronauts from '../images/astronauts.svg';
+import { Link } from 'react-router-dom';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar/>
+                <div className="Home__hero">
+                    <div className="Home__main">
+                        <img className="img-fluid" src={confLogo} alt="Logo" />
+                        <h1>PRINT YOUR BADGES</h1>
+                        <p>The easiest way to manage your <br/>conference </p>
+                        <Link to="/badges/new" type="button" className="btn btn-primary Home__main-button">Start now</Link>
+                    </div>
+                    <div className="Home__main-img">
+                        <img src={Astronauts} />
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    };
+}
+
+export default Home;
